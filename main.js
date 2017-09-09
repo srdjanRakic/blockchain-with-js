701c870c9730d306d791c6239d6ff63ca6db4d2e
srdjanRakic <srg_ian@hotmail.com>
2017-09-09 23:05:41 +0200

    Minor fixes.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const SHA256 = require('crypto-js/sha256');
 class Block {
     constructor (index, timestamp, data, previousHash = '') {
         this.index = index;
-        this.timestamp = indtimestampex;
+        this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
@@ -24,7 +24,7 @@ class Blockchain {
     }
 
     getLatestBlock () {
-        return this.chain(this.chain.length - 1);
+        return this.chain[this.chain.length - 1];
     }
 
     addBlock (newBlock) {
@@ -32,4 +32,25 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid () {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
+
+let srdjanCoin = new Blockchain();
+srdjanCoin.addBlock(new Block(1, '10/09/2017', { amount: 4}));
+srdjanCoin.addBlock(new Block(2, '12/09/2017', { amount: 14}));
